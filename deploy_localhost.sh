23983ef4c3fb313de9fb551730b562f5b5dfcca2
yanglukuan <yanglukuan@gmail.com>
2017-08-30 18:12:57 +0800

    hashmap 调整目录

--- deploy_localhost.sh
+++ deploy_localhost.sh
@@ -1,4 +1,7 @@
+echo -e "\033[40;33m hexo clean................. \033[0m"
 hexo clean
+echo -e "\033[40;33m hexo generate................. \033[0m"
 hexo generate
+echo -e "\033[40;33m start localhost server............. \033[0m"
 start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:4000/
 hexo server
